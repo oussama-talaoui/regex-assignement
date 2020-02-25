138759ae28339e44730b35bd67d0cb9d6a358c52
oussama-talaoui <tahiri.oussup@gmail.com>
2020-02-25 15:29:50 +0100

    Modifs

--- submission_template.py
+++ submission_template.py
@@ -1,25 +1,23 @@
+
 """This is a template for for developing the code of Regex exercise
-
+    
     Remember to rename this filename using the following convention:
-
+    
         syslog_analysis_group-<groupNumber>.py
-
+    
     """
 # The first thing to do is to import the regexp module into our script
 import re
-import string  # not used yet
-
 
 def exercise1(fileName="messages_syslog_class.txt"):
     """Compute ranking of usernames according to number of login attempts
-
+        
         Keyword arguments:
         fileName -- path + name of file containing ssh system log events
         """
-    # TODO: use fileName
-    #   get the text file from a relative path that can run everywhere
+
     # Opening the file in read mode
-    text = open("messages_syslog_class.txt", "r")
+    text = open(fileName, "r")
     frequency = {}
     text_string = text.read()
     failed = re.findall(r'Failed\spassword\sfor\sinvalid\suser\s(?P<user>.*?) ', text_string)
@@ -37,18 +35,16 @@ def exercise1(fileName="messages_syslog_class.txt"):
 
     # Function body
 
-
 def exercise2(fileName="./messages_syslog_class.txt"):
     """Compute the distribution of ssh attacks and IP addresses
-
+        
         Keyword arguments:
         fileName -- path + name of file containing ssh system log events
         """
-    text = open("messages_syslog_class.txt", "r")
+    text = open(fileName, "r")
     frequency = {}
     text_string = text.read()
-    match_pattern = re.findall(r'Failed\spassword\sfor\sinvalid\suser\s(?P<user>.*?)\sfrom\s(?P<IPaddr>.*?)\s',
-                               text_string)
+    match_pattern = re.findall(r'Failed\spassword\sfor\sinvalid\suser\s(?P<user>.*?)\sfrom\s(?P<IPaddr>.*?)\s', text_string)
     IPadrs = [x[1] for x in match_pattern]
 
     for IPadr in IPadrs:
@@ -62,30 +58,26 @@ def exercise2(fileName="./messages_syslog_class.txt"):
     num_of_attacks_by_IP = sorted(num_of_attacks_by_IP)
 
     max_num_attack = max(num_of_attacks_by_IP)
-    # print(num_of_attacks_by_IP)
+    #print(num_of_attacks_by_IP)
     hist = {}
     quarter = 0
     for i in range(len(num_of_attacks_by_IP)):
-        if (num_of_attacks_by_IP[i] > (quarter + 1) * max_num_attack / 4):
+        if(num_of_attacks_by_IP[i] > (quarter+1)*max_num_attack/4):
             quarter += 1
-        hist[quarter + 1] = hist.get(quarter + 1, 0) + 1
+        hist[quarter+1] = hist.get(quarter+1, 0) + 1
 
-    # print(hist)
-
-    list = [(str((k - 1) * max_num_attack / 4) + " < x <= " + str(k * max_num_attack / 4), v) for k, v in hist.items()]
-    # print(list)
+    list = [(str((k-1)*max_num_attack/4)+" < x <= " + str(k*max_num_attack/4), v) for k, v in hist.items()]
 
     return list
 
-
 def exercise3(fileName="./messages_syslog_class.txt"):
     """Obtain a Ranking of failed login attempts by source IP
-
+        
         Keyword arguments:
         fileName -- path + name of file containing ssh system log events
         """
-
-    text = open("messages_syslog_class.txt", "r")
+    
+    text = open(fileName, "r")
     frequency = {}
     text_string = text.read()
     match_pattern = re.findall(r'Failed\spassword\sfor\sinvalid\suser\s(?P<user>.*?)\sfrom\s(?P<IPaddr>.*?)\s',
@@ -102,15 +94,13 @@ def exercise3(fileName="./messages_syslog_class.txt"):
 
     return list
 
-
 def exercise4(fileName="./messages_syslog_class.txt"):
     """Compute the reverse ranking of average period between login attempts (in seconds)
-
+        
         Keyword arguments:
         fileName -- path + name of file containing ssh system log events
         """
 
     # Function body
-
 
 print(exercise3())
